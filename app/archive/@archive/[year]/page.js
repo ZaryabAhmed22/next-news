573206933abb16b23eea4733fcd16d12aa0e64b1
Zaryab Ahmed Khan <zaryabzak22@gmail.com>
2025-03-09 18:13:02 +0500

    add [[...filter]] catchall route

--- app/archive/@archive/[year]/page.js
+++
@@ -1,8 +0,0 @@
-import NewsList from "@/components/news-list";
-import { getNewsForYear } from "@/lib/news";
-import React from "react";
-
-export default function FilteredNewsPage({ params }) {
-  const filteredNews = getNewsForYear(params.year);
-  return <NewsList news={filteredNews} />;
-}
